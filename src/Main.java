c917666c4918cb3f1a64efd28b2f2c0e3dde346a
Blicker777 <lordid@rambler.ru>
2017-02-16 14:05:38 +0300

    Создано поле массива символов

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 /**
  * Created by Igor on 16.02.2017.
  */
 public class Main {
+
+    final static int X_SIZE = 3;
+    final static int Y_SIZE = 3;
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        char[][] field = new char[Y_SIZE][X_SIZE];
+
+    }
 }
